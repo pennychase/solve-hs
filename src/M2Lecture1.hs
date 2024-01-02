a535c0c8f36bb7900f1118aa342912cd772a1b5f
Penny Chase <mpchase@mac.com>
2024-01-01 19:34:22 -0500

    Module 2 Lecture 1 - compatrmentalize and toggleLights

--- src/M2Lecture1.hs
+++ src/M2Lecture1.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module M2Lecture1 where
 
 import Control.Monad.Fail
@@ -8,7 +9,26 @@ import Data.List (sort)
 import Utils
 
 compartmentalize :: (MonadFail m) => String -> Int -> m Char
-compartmentalize input numBuckets = undefined
+compartmentalize [] _ = fail "Empty input"
+compartmentalize input numBuckets = 
+  case quotRem (length input) numBuckets of
+    (len, 0) -> findIntersection len input
+    (_, _) -> fail "Input is not a multiple of numBuckets"
+    where
+      findIntersection :: (MonadFail m) => Int -> String -> m Char
+      findIntersection len input
+        | S.null i = fail "No common character"
+        | S.size i > 1 = fail "Multiple commmon characters"
+        | otherwise = pure $ head $ S.elems i
+        where
+          cs = chunks len input
+          i = foldr S.intersection (head cs) (tail cs)
+      chunks :: Int -> String -> [S.Set Char]
+      chunks _ [] = []
+      chunks n xs = S.fromList chunk : chunks n rest
+        where
+          (chunk, rest) = splitAt n xs
+
 
 traverse2D4P :: Coord2 -> [Direction4] -> Int
 traverse2D4P start dirs = undefined
@@ -17,7 +37,17 @@ data LightCommand = On | Off | Toggle
   deriving (Show, Eq)
 
 toggleLights :: [(LightCommand, Word, Word)] -> Int
-toggleLights = undefined
+toggleLights cmds = 
+  S.size $ foldl process S.empty cmds
+  where
+    process :: S.Set Word -> (LightCommand, Word, Word) -> S.Set Word
+    process onLights (On, start, end)  = S.fromList [start .. end] `S.union` onLights
+    process onLights (Off, start, end) = onLights `S.difference` S.fromList [start .. end]
+    process onLights (Toggle, start, end) = (onLights `S.union` turnOn) `S.difference` turnOff
+      where
+        changes = S.fromList [start .. end]
+        turnOff = onLights `S.intersection` changes
+        turnOn = changes `S.difference` turnOff
 
 treeVisibility :: (MonadLogger m) =>
   (Coord2, Coord2, Coord2) -> ([Coord2], [Coord2], [Coord2]) -> m Int
