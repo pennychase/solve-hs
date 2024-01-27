71ec1c8ddd4f54f2e937d0a460d9ad7ee35d868e
Penny Chase <mpchase@mac.com>
2024-01-27 07:47:59 -0500

    Module 2 Lecture 3 - matrix multiplication, refactored matrix addition (add' calls add), and error checking for set.

--- src/M2Lecture3.hs
+++ src/M2Lecture3.hs
@@ -72,7 +72,10 @@ empty :: Word -> Word -> SparseMatrix
 empty numRows numCols = SparseMatrix numRows numCols M.empty
 
 set :: Word -> Word -> Double -> SparseMatrix -> SparseMatrix
-set row col val sparseMatrix = sparseMatrix { values = values' }
+set row col val sparseMatrix = 
+  if outOfBounds row col sparseMatrix
+    then error $ "Index is out of bounds: " <> show (row, col)
+    else sparseMatrix { values = values' }
   where
     values' = if val == 0.0
                 then M.delete (row, col) (values sparseMatrix)
@@ -85,7 +88,7 @@ outOfBounds row col sparseMatrix =
 get :: Word -> Word -> SparseMatrix -> Double
 get row col sparseMatrix = 
   if outOfBounds row col sparseMatrix
-    then error "Index is out of bounds"
+    then error $ "Index is out of bounds: " <> show (row, col)
     else case M.lookup (row, col) (values sparseMatrix) of
       Nothing -> 0.0
       Just val -> val
@@ -99,16 +102,50 @@ get' row col sparseMatrix =
       Just val -> Just val
 
 add :: SparseMatrix -> SparseMatrix -> SparseMatrix
-add matrix1 matrix2 = undefined
-
+add matrix1 matrix2 =
+  if (nRows matrix1 /= nRows matrix2 || nCols matrix1 /= nCols matrix2)
+    then error "Matrices do not have the same dimensions"
+    else matrix1 { values = M.unionWith (+) (values matrix1) (values matrix2)}
+  
 add' :: SparseMatrix -> SparseMatrix -> Maybe SparseMatrix
-add' matrix1 matrix2 = undefined
+add' matrix1 matrix2 = 
+    if (nRows matrix1 /= nRows matrix2 || nCols matrix1 /= nCols matrix2)
+      then Nothing
+      else Just $ add matrix1 matrix2
 
+-- For each row, compute the dot products with each column in the second matrix that has data
+-- Each column is constructed by transposing the row and using the column number, i.e., the column's rows  
+-- are the columns of the row and the column's columns is the column number
 multiply :: SparseMatrix -> SparseMatrix -> SparseMatrix
-multiply matrix1 matrix2  = undefined
+multiply matrix1 matrix2 = 
+  if nRows matrix1 /= nCols matrix2
+    then error "Number of rows of first matrix not equal to number of columns of second"
+    else foldr (\r ms -> multRow r colNums ms) (empty (nRows matrix1) (nCols matrix2)) rows
+
+  where
+
+    rows = L.groupBy (\x y -> fst x == fst y) $ M.keys (values matrix1)
+    colNums = L.nub $ map snd (M.keys (values matrix2))
+
+    makeColumn row colNum = zip (map snd row) (repeat colNum)
+
+    dotProd row col = sum $ zipWith (\(i1,j1) (i2, j2) -> (get i1 j1 matrix1) * (get i2 j2 matrix2)) row col
+
+    multRow :: [(Word, Word)] -> [Word] -> SparseMatrix -> SparseMatrix
+    multRow row cns matrix = foldr f matrix cns
+      where
+        f cn m =
+          let
+            rn = (fst . head) row
+            val = dotProd row (makeColumn row cn)
+          in set rn cn val m
+
 
 multiply' :: SparseMatrix -> SparseMatrix -> Maybe SparseMatrix
-multiply' matrix1 matrix2 = undefined
+multiply' matrix1 matrix2 = 
+    if nRows matrix1 /= nCols matrix2
+    then Nothing
+    else Just $ multiply matrix1 matrix2
 
 data Op = Add | Sub | Mult | Div
   deriving (Show, Eq)
