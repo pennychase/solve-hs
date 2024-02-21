9a5757699bc37a28b0bfa30979e18a828d2f0ee4
Penny Chase <mpchase@mac.com>
2024-02-21 07:08:51 -0500

    Additional comments to edgePower

--- src/M2Lecture4.hs
+++ src/M2Lecture4.hs
@@ -58,12 +58,16 @@ powerStrikes (numRows, numCols) strikes = undefined
 -- edgePower
 -- To run in cabal repl, use: runStdoutLoggerT $ edgePower edges node
 
--- edgePower implements breadth first traversa; and edgePower' implements depth first
+-- edgePower implements breadth first traversal and edgePower' implements depth first
 -- Use an infinite list of [product, sum] to pass the combination function at each level
--- (apply the head at the current level and pass the tail of the listb to the recursion)
+-- (apply the head at the current level and pass the tail of the list to the recursion)
 
 -- Need to keep the nodes we've seen to avoid cycles amnd track of the edges we've seen to
 -- avoid recomputation.
+
+-- Note that f, the function that applies the computation to the edges of the current node, is
+-- is the same for both depth first and breadth first. g, which controls the order of visiting
+-- the neighborts, differs.
 
 -- Breadth First 
 edgePower :: (MonadLogger m) => [(String, String, Int)] -> String -> m Int
@@ -73,6 +77,11 @@ edgePower edges start = do
     
     graph = mkUndirectedGraph edges
 
+    -- f arguments: node functions edgesVisited nodesVisited accumulator
+    -- f applies (head functions) to node's edges.
+    -- Then calls g to visit node's neighbors, passing the edgesVisited and nodesVisted
+    -- that result from the application, adding the computation to the accumulator,
+    -- and removing the first function (so the next function will be used at the next level)
     f n fns edgesVisited nodesVisited accum =
       if HS.member n nodesVisited 
         then (accum, edgesVisited, nodesVisited)
@@ -86,6 +95,9 @@ edgePower edges start = do
           in
             g (graphNeighbors graph n) (tail fns) edgesVisited' nodesVisited' (accum+result)
 
+    -- g arguments: nodes functions edgesVisited nodesVisited accumulator
+    -- g uses foldr to apply f to all of the nodes.
+    -- Then calls g on (tail nodes), passing the results of the fold
     g [] _ edgesVisited nodesVisited accum = (accum, edgesVisited, nodesVisited)
     g nodes@(n:ns) fns edgesVisited nodesVisited accum = 
         let (result, edgesVisited', nodesVisited') = 
@@ -113,8 +125,11 @@ edgePower' edges start = do
             edgesVisited' = foldr HS.insert edgesVisited (concatMap ((\y -> [(n, y), (y, n)]) . fst) neighbors)
             nodesVisited' = HS.insert n nodesVisited
           in
-            trace ("Node: " <> show n <> ", Result: " <> show result) (g (graphNeighbors graph n) (tail fns) edgesVisited' nodesVisited' result)
-
+            g (graphNeighbors graph n) (tail fns) edgesVisited' nodesVisited' result
+            
+    -- g arguments: nodes functions edgesVisited nodesVisited accumulator
+    -- g applies f to the current node.
+    -- Then calls g on (tail nodes), passing the results of the application
     g [] _ edgesVisited nodesVisited accum = (accum, edgesVisited, nodesVisited)
     g (n:ns) fns edgesVisited nodesVisited accum = 
         let (result, edgesVisited', nodesVisited') = f n fns edgesVisited nodesVisited
